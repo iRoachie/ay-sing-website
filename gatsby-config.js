9c574b51f192e6338403a1da2eb81872c8e87223
Kyle Roach <kroach.work@gmail.com>
2019-07-25 06:55:43 -0400

    build: Use gatsby plugin for purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,17 @@ module.exports = {
     },
     'gatsby-plugin-offline',
     {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        // printRejected: true, // Print removed selectors and processed file names
+        develop: false, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-80535053-2',
